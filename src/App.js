3b086601fe54738065dc81a01f81a45ce79b7931
Batuhan Zorbey Zengin <batuhanzorbeyzengin@192.168.1.108>
2023-01-06 03:26:50 +0300

    dev

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+//import { useEffect } from "react";
+import { PostService } from "./services";
+import { useQuery } from "react-query";
+
+const formData = {
+  userId: 3,
+  title: 'test',
+  body: 'test'
+}
 
 function App() {
+
+  // useEffect(() => {
+  //   PostService.getPosts().then(res => console.log(res));
+  //   PostService.getPostDetail(2).then(res => console.log(res));
+  //   PostService.newPost({
+  //     userId: 3,
+  //     title: 'test',
+  //     body: 'test'
+  //   }).then(res => console.log(res));
+  // })
+
+  const getPosts = useQuery(
+    ["getPosts"],
+    () => PostService.getPosts(),
+    { staleTime: 60000 }
+  );
+
+  console.log("getPost data: ", getPosts.data);
+
+  const getPostDetail = useQuery(
+    ["getPostDetail"],
+    () => PostService.getPostDetail(2),
+    { staleTime: 60000 }
+  );
+
+  console.log("getPostDetail data: ", getPostDetail.data);
+
+  const newPost = useQuery(
+    ["newPost"],
+    () => PostService.newPost(formData),
+    { staleTime: 60000 }
+  );
+
+  console.log("New Post data: ", newPost.data);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    
+    </>
   );
 }
 
